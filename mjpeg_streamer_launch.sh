02856420ce8f99793da50bedcf181f7a02e902a8
DK <13267481+Dan-Kat@users.noreply.github.com>
2022-12-08 16:32:24 -0800

    Update mjpeg_streamer_launch.sh

--- mjpeg_streamer_launch.sh
+++ mjpeg_streamer_launch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 killall mjpg_streamer
-cd /home/picard/mjpg-streamer/mjpg-streamer-experimental && mjpg_streamer -o "output_http.so -w ./www" -i "input_uvc.so -d /dev/video0 -r 1280x720 -n"
+cd /home/picard/mjpg-streamer/mjpg-streamer-experimental && mjpg_streamer -o "output_http.so -w ./www -p 8081" -i "input_uvc.so -d /dev/video0 -r 1280x720 -n"
